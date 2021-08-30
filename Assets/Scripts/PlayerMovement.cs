e2cdee5f99b0609ef7eb59596965ef343a90a323
Drigolino <rodrigolino05@hotmail.com>
2021-08-29 22:23:40 -0400

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    SpriteRenderer spriteRender;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        spriteRender.flipX = movement.x < 0;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+movement*moveSpeed*Time.fixedDeltaTime);
+    }
+}
