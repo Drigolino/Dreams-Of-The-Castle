1ac36e97b39a64c0eae86d8082cbe6c06b12a06b
Drigolino <rodrigolino05@hotmail.com>
2021-09-01 09:50:14 -0400

    AI Enemigo2Cercano

---
+++ Assets/Scripts/Ai2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.Accessibility;
+
+
+public class Ai2 : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    public float stopingDistance;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position,target.position)>stopingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,target.position,speed*Time.deltaTime);
+        }
+    }
+}
