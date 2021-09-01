4ca5ea9b0ee9d7e43f069486b53e0f6cc26bb149
Drigolino <rodrigolino05@hotmail.com>
2021-09-01 14:23:44 -0400

    Arreglo Normalized

--- Assets/Scripts/BalaDireccion.cs
+++ Assets/Scripts/BalaDireccion.cs
@@ -7,7 +7,7 @@ public class BalaDireccion : MonoBehaviour
     public GameObject myPlayer;
     private void FixedUpdate()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position.normalized;
         difference.Normalize();
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
